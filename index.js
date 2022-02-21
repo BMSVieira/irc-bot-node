7498cfb9b69ee9b1c8913de18779c27ecd7af936
Bruno Vieira <brunobmsv@gmail.com>
2022-02-21 11:56:04 +0000

    Update index.js

--- index.js
+++ index.js
@@ -158,6 +158,120 @@ var frases = [
     },
     {
       frase: "{nick} Vou mandar embora todos os que medem menos de 1,60. Isto aqui não é um formigueiro."
+    },
+    {
+      frase: "{nick} Algumas coisas tem preço, outras tem valor."
+    },
+    {
+      frase: "{nick} Beijar é a terceira coisa mais linda que o ser humano faz com a boca. A segunda é comer e a primeira é ficar calado."
+    },
+    {
+      frase: "Quando olho para ti {nick}, lembras a minha conta corrente... Fico à espera que aconteça um milagre."
+    },
+    {
+      frase: "{nick} Um dia alguém disse 'Jamais me esquecerás'. Só não lembro quem foi. "
+    },
+    {
+      frase: "Meu silêncio vai te mandar o troco {nick}."
+    },
+    {
+      frase: "{nick} Já estava farto do cheiro a lavanda, pinho e limão da sala. Então, pendurei chouriços, morcelas e alheiras."
+    },
+    {
+      frase: "{nick} Se tudo que já ofereci não serve de nada, está na hora de oferecer minha ausência."
+    },
+    {
+      frase: "{nick} Existem finais felizes e finais necessários."
+    },
+    {
+      frase: "Sou como o açúcar {nick}: mesmo sob pressão, mesmo sob tortura, ainda faço o bem, ainda destilo doçura. É mentira mas, foi profundo."
+    },
+    {
+      frase: "{nick} Disseste que ias me levar a um sítio romântico e afinal... enfiou-me o dia todo no McDonald's."
+    },
+    {
+      frase: "{nick} Todos a namorar nas moitas e eu aqui a pensar como entra água dentro dos côcos e pevides nas abóboras."
+    },
+    {
+      frase: "Estava aqui a pensar {nick}, que com alguns prints estragava a felicidade a muita gente."
+    },
+    {
+      frase: "{nick} Fui atrás da felicidade e voltei com uma chávena de café."
+    },
+    {
+      frase: "Eu percebo tudo {nick} mas, fico em silêncio, não falo nada."
+    },
+    {
+      frase: "As vezes a única maneira de permanecer são, é enlouquecer {nick}."
+    },
+    {
+      frase: "{nick} Paciência não é o meu forte. Então, não abusa."
+    },
+    {
+      frase: "{nick} Meu nutricionista disse que eu sou o que como. Estou à base de pizza, doces e chocolates, etc. Daí concluo que, sou muito gostoso."
+    },
+    {
+      frase: "Às vezes {nick}, os milagres são as pessoas boas que aparecem no nosso caminho."
+    },
+    {
+      frase: "{nick} Hoje é aquele dia que vou fazer tudo, inclusive nada."
+    },
+    {
+      frase: "Inveja não sei do quê, {nick}. Até parece que a tua pobreza é melhor que a minha..."
+    },
+    {
+      frase: "Quem pensas que és para afirmar que não vivo sem ti, {nick}? Por acaso, és café?"
+    },
+    {
+      frase: "{nick} Falam de mim porque se falassem de vocês ninguém teria interesse."
+    },
+    {
+      frase: "{nick} Confia no homem que tenhas depilação melhor que a tua..."
+    },
+    {
+      frase: "Não me importo se é alta, baixa, magra, rica ou pobre {nick}. No final do dia... é noite!"
+    },
+    {
+      frase: "{nick} A única coisa que o tempo cura, é o presunto."
+    },
+    {
+      frase: "{nick} Queres sentir borboletas no estômago? Começa já a comer bichos de seda."
+    },
+    {
+      frase: "Bebo para afogar as mágoas {nick} mas, as filhas da mãe sabem nadar... "
+    },
+    {
+      frase: "Triste não é mudar de ideias, {nick}. Triste é não ter ideias para mudar."
+    },
+    {
+      frase: "{nick} Errar é humano. Culpar outra pessoa é política."
+    },
+    {
+      frase: "Comecei uma dieta, {nick}. Cortei bebidas e comidas pesadas. Em catorze dias, perdi duas semanas."
+    },
+    {
+      frase: "{nick} Eu evito coisas que engordam, tipo espelhos, balanças e fotografias."
+    },
+    {
+      frase: "{nick} A frase mais bipolar do mundo: Minha nossa!"
+    },
+    {
+      frase: "{nick} As aparências iludem e em alguns casos, assustam."
+    },
+    {
+      frase: "Aprendi a nunca discutir com um idiota, {nick}. Ele ganha sempre."
+    },
+    {
+      frase: "{nick} Se eu sou bom partido, imagina inteiro."
+    },
+    {
+      frase: "{nick} Segue o teu sonho... Continua a dormir."
+    },
+    {
+      frase: "{nick} Eu nem quero mais conversa."
+    },
+    {
+      frase: "Vai pastar {nick}.."
     }
 ];
 
