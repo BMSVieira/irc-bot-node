c82fbf027041b7070eb67cde94b6f28385f30dc2
Bruno Vieira <brunobmsv@gmail.com>
2022-12-10 02:49:13 +0000

    Update index.js

--- index.js
+++ index.js
@@ -950,7 +950,7 @@ var freenode = irc.connect('167.114.210.155', ircOptions)
         console.log(cname, names);
     })
 
-    freenode.on('Welcome to Brazink!', function (msg) {
+    freenode.on('NOTICE', function (msg) {
 
         // Keep alive enviado.
         this.on('PING', function (evt) {
