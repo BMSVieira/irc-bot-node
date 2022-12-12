9c7f5369795b7587260dd7adeda7d1d0c531c23d
Bruno Vieira <brunobmsv@gmail.com>
2022-12-12 22:04:32 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
     var irc = require('irc');
+    var myadd = 0;
+
+require('dns').lookup(require('os').hostname(), function (err, add, fam) {
+   myadd = add;
+})
+
+
 
     var bot = new irc.Client('irc.ptnet.org','MeMario', {
         userName: 'node',
         realName: 'nodeJS IRC client',
         port: 6697,
-        localAddress: "176.9.29.68",
+        localAddress: "172.31.128.14",
         debug: true,
         showErrors: true,
         autoRejoin: true,
@@ -25,4 +32,4 @@
     });
 
 
-
+console.log(bot);
