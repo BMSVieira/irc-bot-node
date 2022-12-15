8ae0be36dd1bab75699bd6115e34598e1717a806
Bruno Vieira <brunobmsv@gmail.com>
2022-12-15 20:47:40 +0000

    update

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-    process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
-
     // ****************************************************************
     // IRC BOT
     // ****************************************************************
@@ -30,7 +28,7 @@
         channels: [core.config[0]["global_channel"]],
         secure: true,
         selfSigned: true,
-        certExpired: false,
+        certExpired: true,
         floodProtection: false,
         floodProtectionDelay: 1000,
         sasl: false,
